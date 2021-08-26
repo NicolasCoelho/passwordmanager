7412a181a076acc4984f150628f119578b501d24
NicolasCoelho <nicolas_mansan@hotmail.com>
2021-08-26 18:42:03 -0300

    Intro form validation; Intro animations

--- src/app/pages/intro/intro.component.ts
+++ src/app/pages/intro/intro.component.ts
@@ -1,10 +1,23 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, NgModule, OnInit, ViewEncapsulation } from '@angular/core';
 import { User } from 'src/app/_interfaces/user';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
+  animations: [
+    trigger('InOut', [
+      transition(':enter', [
+        style({ opacity: 0, margin: '0px 0px -100% 0px'}),
+        animate('300ms ease-out', style({opacity: 1, margin: 0 }) )
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, margin: 0, position: 'absolute' }),
+        animate('300ms ease-in', style({opacity: 0, margin: '0px 0px 100% 0px' }))
+      ])
+    ])
+  ]
 })
 export class IntroComponent implements OnInit {
 
@@ -14,10 +27,17 @@ export class IntroComponent implements OnInit {
 
   confirmPassword: String;
 
+  public errors: any;
+
   constructor() {
     this.step = 1;
     this.user = {name: '', password: ''};
     this.confirmPassword = '';
+    this.errors = {
+      name: false,
+      password: false,
+      confirmPassword: false
+    }
   }
 
   ngOnInit(): void {
@@ -36,6 +56,10 @@ export class IntroComponent implements OnInit {
   }
 
   validateInputs() {
+    this.errors.name = (this.user.name.length === 0)
+    this.errors.password = (this.user.password === '' || this.user.password.length < 8);
+    this.errors.confirmPassword = (this.confirmPassword !== this.user.password && this.user.password.length > 0)
+
     if(this.user.name === '' || this.user.password === '' || this.confirmPassword === '') return false;
     return (this.confirmPassword === this.user.password && this.user.password.length >= 8);
   }
